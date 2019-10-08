04e2bbdbd31402d00eb1089f5a597ab243a2344b
reus <reusee@gmail.com>
2019-10-08 21:31:25 +0800

    Revert "Revert "add wire encryption""
    This reverts commit 19cccad58690abe6f2dc867d24189308bc44c945.

--- wire.go
+++ wire.go
@@ -1,16 +1,30 @@
 package l2
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"encoding/binary"
+	"fmt"
 	"io"
+	"math/rand"
 )
 
 type Outbound struct {
-	Eth         Bytes
-	IsBroadcast bool
-	DestNode    *Node
-	Serial      uint64
+	Eth          Bytes
+	IsBroadcast  bool
+	DestNode     *Node
+	Serial       uint64
+	PreferFormat WireFormat
 }
+
+type WireFormat uint8
+
+const (
+	FormatAESGCM WireFormat = iota
+
+	AESGCMNonceSize = 12
+)
 
 type Inbound struct {
 	Eth         Bytes
@@ -19,27 +33,119 @@ type Inbound struct {
 }
 
 func (n *Network) writeOutbound(w io.Writer, outbound *Outbound) error {
-	if err := binary.Write(w, binary.LittleEndian, uint16(len(outbound.Eth.Bytes))); err != nil {
+	plaintextBS := getBytes(
+		2 + // payload len
+			len(outbound.Eth.Bytes) + // payload
+			8, // serial
+	)
+	defer plaintextBS.Put()
+	buf := bytes.NewBuffer(plaintextBS.Bytes[:0])
+	if err := binary.Write(buf, binary.LittleEndian, uint16(len(outbound.Eth.Bytes))); err != nil {
 		return err
 	}
-	_, err := w.Write(outbound.Eth.Bytes)
+	_, err := buf.Write(outbound.Eth.Bytes)
 	if err != nil {
 		return err
 	}
-	if err := binary.Write(w, binary.LittleEndian, outbound.Serial); err != nil {
+	if err := binary.Write(buf, binary.LittleEndian, outbound.Serial); err != nil {
 		return err
 	}
+	plaintext := buf.Bytes()
+	switch outbound.PreferFormat {
+
+	case FormatAESGCM:
+
+		block, err := aes.NewCipher(n.CryptoKey)
+		ce(err)
+		aead, err := cipher.NewGCM(block)
+		ce(err)
+		nonce := make([]byte, AESGCMNonceSize)
+		for i := 0; i+8 <= AESGCMNonceSize; i += 4 {
+			binary.LittleEndian.PutUint64(nonce[i:i+8], rand.Uint64())
+		}
+		buf := getBytes(
+			1 + // format
+				AESGCMNonceSize + // nonce
+				len(plaintext) + // plaintext
+				aead.Overhead(), // overhead
+		)
+		defer buf.Put()
+		buf.Bytes[0] = byte(FormatAESGCM)
+		copy(
+			buf.Bytes[1:1+AESGCMNonceSize],
+			nonce,
+		)
+		ciphertext := aead.Seal(
+			buf.Bytes[1+AESGCMNonceSize:1+AESGCMNonceSize],
+			nonce,
+			plaintext,
+			nil,
+		)
+		buf.Bytes = buf.Bytes[:1+AESGCMNonceSize+len(ciphertext)]
+		if err := binary.Write(w, binary.LittleEndian, uint16(len(buf.Bytes))); err != nil {
+			return err
+		}
+		if _, err := w.Write(buf.Bytes); err != nil {
+			return err
+		}
+
+	}
+
 	return nil
 }
+
+var errBadFrame = fmt.Errorf("bad frame")
 
 func (n *Network) readInbound(r io.Reader) (inbound Inbound, err error) {
 	var l uint16
 	if err = binary.Read(r, binary.LittleEndian, &l); err != nil {
 		return
 	}
+	ciphertextBs := getBytes(int(l))
+	defer ciphertextBs.Put()
+	if _, err = io.ReadFull(r, ciphertextBs.Bytes); err != nil {
+		return
+	}
+	ciphertext := ciphertextBs.Bytes
+
+	if len(ciphertext) == 0 {
+		err = errBadFrame
+		return
+	}
+
+	var plaintext []byte
+	switch WireFormat(ciphertext[0]) {
+
+	case FormatAESGCM:
+		ciphertext = ciphertext[1:]
+		if len(ciphertext) < AESGCMNonceSize {
+			err = errBadFrame
+			return
+		}
+		nonce := ciphertext[:AESGCMNonceSize]
+		ciphertext = ciphertext[AESGCMNonceSize:]
+		block, e := aes.NewCipher(n.CryptoKey)
+		ce(e)
+		aead, e := cipher.NewGCM(block)
+		ce(e)
+		plaintext, err = aead.Open(ciphertext[:0], nonce, ciphertext, nil)
+		if err != nil {
+			return
+		}
+
+	}
+
+	r = bytes.NewReader(plaintext)
+	if err = binary.Read(r, binary.LittleEndian, &l); err != nil {
+		return
+	}
 	bs := getBytes(int(l))
+	defer func() {
+		if err != nil {
+			bs.Put()
+		}
+	}()
 	if _, err = io.ReadFull(r, bs.Bytes); err != nil {
-		bs.Put()
 		return
 	}
 	inbound.Eth = bs
