f0162f46a6304856479373d135ebf22221a24218
reus <reusee@gmail.com>
2019-10-15 11:39:26 +0800

    update gogoprotobuf

--- wire_data.pb.go
+++ wire_data.pb.go
@@ -22,7 +22,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type WireData struct {
 	Eth    []byte `protobuf:"bytes,1,opt,name=Eth" json:"Eth"`
@@ -43,7 +43,7 @@ func (m *WireData) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 		return xxx_messageInfo_WireData.Marshal(b, m, deterministic)
 	} else {
 		b = b[:cap(b)]
-		n, err := m.MarshalTo(b)
+		n, err := m.MarshalToSizedBuffer(b)
 		if err != nil {
 			return nil, err
 		}
@@ -99,7 +99,7 @@ var fileDescriptor_bd9a994a62fb525b = []byte{
 func (m *WireData) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
-	n, err := m.MarshalTo(dAtA)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
 	if err != nil {
 		return nil, err
 	}
@@ -107,30 +107,38 @@ func (m *WireData) Marshal() (dAtA []byte, err error) {
 }
 
 func (m *WireData) MarshalTo(dAtA []byte) (int, error) {
-	var i int
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *WireData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
 	_ = i
 	var l int
 	_ = l
-	if m.Eth != nil {
-		dAtA[i] = 0xa
-		i++
-		i = encodeVarintWireData(dAtA, i, uint64(len(m.Eth)))
-		i += copy(dAtA[i:], m.Eth)
-	}
-	dAtA[i] = 0x10
-	i++
 	i = encodeVarintWireData(dAtA, i, uint64(m.Serial))
-	return i, nil
+	i--
+	dAtA[i] = 0x10
+	if m.Eth != nil {
+		i -= len(m.Eth)
+		copy(dAtA[i:], m.Eth)
+		i = encodeVarintWireData(dAtA, i, uint64(len(m.Eth)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
 }
 
 func encodeVarintWireData(dAtA []byte, offset int, v uint64) int {
+	offset -= sovWireData(v)
+	base := offset
 	for v >= 1<<7 {
 		dAtA[offset] = uint8(v&0x7f | 0x80)
 		v >>= 7
 		offset++
 	}
 	dAtA[offset] = uint8(v)
-	return offset + 1
+	return base
 }
 func (m *WireData) Size() (n int) {
 	if m == nil {
@@ -266,6 +274,7 @@ func (m *WireData) Unmarshal(dAtA []byte) error {
 func skipWireData(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
+	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -297,10 +306,8 @@ func skipWireData(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
-			return iNdEx, nil
 		case 1:
 			iNdEx += 8
-			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -321,55 +328,30 @@ func skipWireData(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthWireData
 			}
 			iNdEx += length
-			if iNdEx < 0 {
-				return 0, ErrInvalidLengthWireData
-			}
-			return iNdEx, nil
 		case 3:
-			for {
-				var innerWire uint64
-				var start int = iNdEx
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return 0, ErrIntOverflowWireData
-					}
-					if iNdEx >= l {
-						return 0, io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					innerWire |= (uint64(b) & 0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				innerWireType := int(innerWire & 0x7)
-				if innerWireType == 4 {
-					break
-				}
-				next, err := skipWireData(dAtA[start:])
-				if err != nil {
-					return 0, err
-				}
-				iNdEx = start + next
-				if iNdEx < 0 {
-					return 0, ErrInvalidLengthWireData
-				}
-			}
-			return iNdEx, nil
+			depth++
 		case 4:
-			return iNdEx, nil
+			if depth == 0 {
+				return 0, ErrUnexpectedEndOfGroupWireData
+			}
+			depth--
 		case 5:
 			iNdEx += 4
-			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
+		if iNdEx < 0 {
+			return 0, ErrInvalidLengthWireData
+		}
+		if depth == 0 {
+			return iNdEx, nil
+		}
 	}
-	panic("unreachable")
+	return 0, io.ErrUnexpectedEOF
 }
 
 var (
-	ErrInvalidLengthWireData = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowWireData   = fmt.Errorf("proto: integer overflow")
+	ErrInvalidLengthWireData        = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowWireData          = fmt.Errorf("proto: integer overflow")
+	ErrUnexpectedEndOfGroupWireData = fmt.Errorf("proto: unexpected end of group")
 )
