72a676e7b0e59b221ac7d4709b06750184292949
reus <reusee@gmail.com>
2020-04-10 19:30:45 +0800

    Revert "fix icmp"
    This reverts commit 41d0d7d878b0c1241a074314b39a999c99d8501f.

--- bridge_icmp.go
+++ bridge_icmp.go
@@ -81,24 +81,26 @@ func startICMP(
 	inbounds := make(chan ICMPInbound, 1024)
 
 	// local
-	node := network.LocalNode
-	ip := node.wanIP
-	if len(ip) == 0 && len(node.PrivateIP) > 0 {
-		for _, addr := range localAddrs {
-			if ipnet, ok := addr.(*net.IPNet); ok && ipnet.Contains(node.PrivateIP) {
-				ip = node.PrivateIP
-				break
+	var localConn *net.IPConn
+	spawn(scope, func() {
+		node := network.LocalNode
+		ip := node.wanIP
+		if len(ip) == 0 && len(node.PrivateIP) > 0 {
+			for _, addr := range localAddrs {
+				if ipnet, ok := addr.(*net.IPNet); ok && ipnet.Contains(node.PrivateIP) {
+					ip = node.PrivateIP
+					break
+				}
 			}
 		}
-	}
-	if len(ip) == 0 {
-		return
-	}
-	localConn, err := net.ListenIP("ip4:icmp", &net.IPAddr{
-		IP: net.ParseIP("0.0.0.0"),
-	})
-	ce(err)
-	spawn(scope, func() {
+		if len(ip) == 0 {
+			return
+		}
+		var err error
+		localConn, err = net.ListenIP("ip4:icmp", &net.IPAddr{
+			IP: net.ParseIP("0.0.0.0"),
+		})
+		ce(err)
 		buf := make([]byte, network.MTU*2)
 		for {
 			n, addr, err := localConn.ReadFrom(buf)
